a5f2004767d1a97f0b4227d952550f285f21b7b3
andreatms <65308694+andreatms@users.noreply.github.com>
2022-01-09 21:03:02 +0100

    a

--- operazioni.sql
+++ operazioni.sql
@@ -45,6 +45,12 @@ $BODY$ LANGUAGE plpgsql VOLATILE
 
 
 
+
+
+
+
+
+
 OP12:
 CREATE OR REPLACE FUNCTION modifica_tratta(cap_loc numeric(5), codice_gf int)
 RETURNS void AS
@@ -102,11 +108,88 @@ BEGIN
 FOR j IN 1..10 LOOP
     -- i will take on the values 1,2,3,4,5,6,7,8,9,10 within the loop
 END LOOP;
+-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% --
+
+
+
+CREATE OR REPLACE FUNCTION addSpedizione(CodiceDestinatario INT, NominativoDestinatario VARCHAR(20), CodiceMittente INT, NominativoMittente VARCHAR(20), ViaDestinazione VARCHAR(30), CivicoDestinazione INT, ViaPartenza VARCHAR(30), CivicoPartenza INT, CAPDestinazione num
+(5), CAPPartenza NUMERIC(5), Telefono NUMERIC(10))
+RETURNS void AS
+$BODY$
+
+DECLARE CodiceNuovaSpedizione INT;
+SET CodiceNuovaSpedizione := (SELECt max(Codice)+1 FROM Spedizione);
+
+DECLARE CodicePercorso INT;
+SET CodicePercorso := (SELECT Codice
+                        FROM Percorso
+                        WHERE 
+
+DECLARE Stato VARCHAR(20);
+SET Stato = "in transito";
+
+DECLARE ViaPartenza VARCHAR(30);
+DECLARE CivicoPartenza INT;
+DECLARE Telefono NUMERIC(10);
+
+BEGIN
+    IF $7 <> '1' 
+        THEN
+            LocalitaPartenza = (SELECT CAP
+                                FROM Clienti
+                                WHERE CodiceCliente = $7)
+    ELSE
+
+
 
 
 END
-
 $BODY$ LANGUAGE plpgsql VOLATILE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--pollorosso--
+OP2: Rimuovere una spedizione
+CREATE OR REPLACE FUNCTION removeSpedizione(Codice INT)
+RETURNS void AS
+$BODY$
+DELETE FROM Spedizione WHERE Codice = $1;
+$BODY$ LANGUAGE plpgsql VOLATILES 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -145,6 +228,63 @@ GROUP BY Regione
 $BODY$ LANGUAGE plpgsql VOLATILE
 
 
+
+OP31:
+CREATE OR REPLACE 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 OP37:
 CREATE OR REPLACE FUNCTION printTariffe()
 RETURNS TABLE(CodiceTariffa INT, CostoAnnuo INT, Categoria INT, Prezzo INT, VolumeMinimo INT, VolumeMassimo INT) AS 
@@ -157,9 +297,9 @@ $BODY$ LANGUAGE plpgsql VOLATILE
 
 OP38:Visualizzare tutte le spedizioni effettuate da un cliente 
 CREATE OR REPLACE FUNCTION printSpedizioniByMittente(CodiceCliente INT)
-RETURNS TABLE(CodiceSpedizione INT, NominativoMittente VARCHAR(20) NominativoDestinatario VARCHAR(20), IndirizzoDestinazione VARCHAR(30) CivicoDestinazione INT, Stato VARCHAR(20), Costo INT)
+RETURNS TABLE(CodiceSpedizione INT, NominativoMittente VARCHAR(20) NominativoDestinatario VARCHAR(20), IndirizzoDestinazione VARCHAR(30) CivicoDestinazione INT, Stato VARCHAR(20), Costo INT) AS
 $BODY$
-SELECT CodiceSpedizione, NominativoMittente, NominativoDestinatario, IndirizzoDestinazione, CivicoDestinazione, Stato, Costo
+SELECT Codice, NominativoMittente, NominativoDestinatario, IndirizzoDestinazione, CivicoDestinazione, Stato, Costo
 FROM Spedizione
 WHERE CodiceMittente = $1
 $BODY$ LANGUAGE plpgsql VOLATILE
@@ -167,9 +307,9 @@ $BODY$ LANGUAGE plpgsql VOLATILE
 
 OP39:Visualizzare tutte le spedizioni ricevute da un cliente
 CREATE OR REPLACE FUNCTION printSpedizioniByMittente(CodiceCliente INT)
-RETURNS TABLE(CodiceSpedizione INT, NominativoMittente VARCHAR(20) NominativoDestinatario VARCHAR(20), IndirizzoDestinazione VARCHAR(30) CivicoDestinazione INT, Stato VARCHAR(20), Costo INT)
+RETURNS TABLE(CodiceSpedizione INT, NominativoMittente VARCHAR(20) NominativoDestinatario VARCHAR(20), IndirizzoDestinazione VARCHAR(30) CivicoDestinazione INT, Stato VARCHAR(20), Costo INT) AS
 $BODY$
-SELECT CodiceSpedizione, NominativoMittente, NominativoDestinatario, IndirizzoDestinazione, CivicoDestinazione, Stato, Costo
+SELECT Codice, NominativoMittente, NominativoDestinatario, IndirizzoDestinazione, CivicoDestinazione, Stato, Costo
 FROM Spedizione
 WHERE CodiceDestinatario = $1
 $BODY$ LANGUAGE plpgsql VOLATILE
@@ -177,27 +317,28 @@ $BODY$ LANGUAGE plpgsql VOLATILE
 
 OP40:Visualizzare la lista di tutti i dipendenti presenti in una filiale riportando la sezione in cui lavorano il ruolo ricoperto e lo stipendio
 CREATE OR REPLACE FUNCTION printDipendentiByFiliale(CodiceFiliale INT)
-RETURNS TABLE(Matricola INT, Nome VARCHAR(20), Cognome VARCHAR(20), Ruolo VARCHAR(20), Sezione VARCHAR(10), Stipendio INT)
+RETURNS TABLE(Matricola INT, Nome VARCHAR(20), Cognome VARCHAR(20), Ruolo VARCHAR(20), Sezione VARCHAR(10), Stipendio INT) AS
 $BODY$
 SELECT Dipendente.Matricola, Dipendente.Nome, Dipendente.Cognome, Ruolo.Nome, Sezione.Nome, Ruolo.Stipendio
 FROM Dipendente JOIN Afferenza ON Dipendente.Matricola = Afferenza.Dipendente
-                JOIN Filiale ON Afferenza.Filiale = Filiale.CodiceFiliale
+                JOIN Filiale ON Afferenza.Filiale = Filiale.Codice
                 JOIN Sezione ON Afferenza.Sezione = Sezione.Sigla
-                        AND Filiale.CodiceFiliale = Sezione.Filiale
+                        AND Filiale.Codice = Sezione.Filiale
                 JOIN Impiego ON Dipendente.Matricola = Impiego.Dipendente
                 JOIN Ruolo ON Impiego.Ruolo = Ruolo.Sigla
-WHERE Filiale.CodiceFiliale = $1
+WHERE Filiale.Codice = $1
 $BODY$ LANGUAGE plpgsql VOLATILE
 
 
 OP41:Visualizzare tutti i dipendenti relativi ad un particolare ruolo presenti nell’azienda riportando la filiale in cui operano
 CREATE OR REPLACE FUNCTION printDipendentiByRuolo(Ruolo VARCHAR(20))
-RETURNS TABLE(Matricola INT, Nome VARCHAR(20), Cognome VARCHAR(20), Ruolo VARCHAR(20), Filiale INT)
+RETURNS TABLE(Matricola INT, Nome VARCHAR(20), Cognome VARCHAR(20), Ruolo VARCHAR(20), Filiale INT) AS
 $BODY$
-SELECT Dipendente.Matricola, Dipendente.Nome, Dipendente.Cognome, Ruolo.Nome, Filiale.CodiceFiliale
+SELECT Dipendente.Matricola, Dipendente.Nome, Dipendente.Cognome, Ruolo.Nome, Filiale.Codice
 FROM Dipendente JOIN Afferenza ON Dipendente.Matricola = Afferenza.Dipendente
-                JOIN Filiale ON Afferenza.Filiale = Filiale.CodiceFiliale
+                JOIN Filiale ON Afferenza.Filiale = Filiale.Codice
                 JOIN Impiego ON Dipendente.Matricola = Impiego.Dipendente
                 JOIN Ruolo ON Impiego.Ruolo = Ruolo.Sigla
 WHERE Ruolo.Sigla = $1
 $BODY$ LANGUAGE plpgsql VOLATILE
+
